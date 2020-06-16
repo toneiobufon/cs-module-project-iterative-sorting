70cf54d3db317ed72147bd44880721df199fffd0
toneiojimz <ajimeneza77@gmail.com>
2020-06-15 22:28:55 -0700

    working on searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,6 +9,21 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    lo = 0
+    hi = len(arr)
+
+    #while lo and hi do not overlap
+    while lo < hi:
+        mid = (lo + hi) // 2 # dividing by // eliminates fractions
+
+        if arr[mid] == target:
+            return True
+        
+        elif target < arr[mid]:
+            hi = mid
+
+        else:
+            lo = mid + 1
 
 
-    return -1  # not found
+    return False  # not found
